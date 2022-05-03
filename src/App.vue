a1da79f729595467ae7e97d4237b4498bcad4f34
João Victor Davi <j.victordavid2@gmail.com>
2022-05-03 13:07:48 -0300

    chore: remove HelloWorld component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,14 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld/>
+      <h1></h1>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-}
+  name: "App",
+  components: {},
+};
 </script>
